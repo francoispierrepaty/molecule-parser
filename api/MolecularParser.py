28655ecd2012b6c34a9c10c5e097dac645ee84b2
François-Pierre PATY <francoispierrepaty@MacBook-Pro-de-Francois-Pierre.local>
2021-04-16 12:00:46 +0200

    Initial commit

---
+++ api/MolecularParser.py
@@ -0,0 +1,53 @@
+from flask_restful import Api, Resource, reqparse
+import re
+
+class MolecularParser(Resource):
+
+  def post(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument('formula', type=str)
+    args = parser.parse_args()
+    formula = args['formula']
+    if not self.check_formula(formula):
+      return False
+    atoms = self.parse_formula(formula)
+    return {'atoms': atoms}
+
+  def check_formula(self, formula):
+    count = 0
+    for i in formula:
+      if i in '([{':
+        count += 1
+      elif i in ')]}':
+        count -= 1
+      if count < 0:
+        return False
+    return (count == 0)
+
+  def parse_formula(self, formula):
+    split_formula = re.findall('[A-Z][a-z]?|\d+|.', formula)
+    elements = "H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og".split()
+    count = 1
+    multipliers = [1]
+    atoms = {}
+
+    for part in split_formula[::-1]:
+      if part.isalpha():
+        if part not in elements:
+          return part
+        if part in atoms:
+          atoms[part] += multipliers[-1]*count
+        else:
+          atoms[part] = multipliers[-1]*count
+        count = 1
+      elif part.isdecimal():
+        count = int(part)
+      elif part in '([{':
+        multipliers.pop()
+      elif part in ')]}':
+        multipliers.append(multipliers[-1]*count)
+        count = 1
+      else:
+        return False
+
+    return atoms
---
+++ molecular-parser/src/App.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {formula: '', atoms: false, sentFormula: '', hasAnswered: false, error: false};
+
+    this.compute = this.compute.bind(this);
+    this.handleFormulaChange = this.handleFormulaChange.bind(this);
+  }
+
+  compute(e) {
+    e.preventDefault();
+
+    axios({
+      method: 'post',
+      url: 'https://molecular-parser-joko.herokuapp.com/',
+      headers: {'content-type': 'application/json'},
+      data: {
+        formula: this.state.formula
+      }
+    })
+    .then(result => {
+      this.setState({
+        atoms: result.data.atoms,
+        hasAnswered: true,
+        sentFormula: this.state.formula
+      });
+    })
+    .catch(error => {
+      this.setState({
+        error: error
+      });
+    });
+  }
+
+  handleFormulaChange(e) {
+    this.setState({formula: e.target.value});
+  }
+
+  render() {
+    let atoms = (!this.state.atoms || typeof this.state.atoms === 'string') ? {} : this.state.atoms;
+    const atomsList = Object.entries(atoms).map(([atom,number]) =>
+      <li key={atom}>
+        {atom}: {number}
+      </li>
+    );
+    return (
+      <div>
+        <div className="masthead-brand">
+          <img src="logo512.png" width="100px" alt="Molecular Parser" className="logo" />
+        </div>
+        
+        <div className="container row">
+          <form className="form-signin">
+            <div className="text-center mb-4">
+              <h1 className="h3 mb-3 font-weight-normal">How many atoms are there in my molecule?</h1>
+              <h5>Enter the chemical formula below</h5>
+
+              <div className="form-label-group">
+                <input type="text" name="formula" id="formula" placeholder="Formula" className="form-control"
+                        value={this.state.formula} onChange={this.handleFormulaChange} required autoFocus />
+                <label htmlFor="formula">Formula</label>
+              </div>
+              
+              <button className="btn btn-lg btn-primary btn-block" onClick={this.compute}>Compute the number of each atom</button>
+            </div>
+
+            {this.state.hasAnswered && (!this.state.atoms || typeof this.state.atoms === 'string') ?
+              <div className="alert span5 alert-danger">
+                <h5>Invalid Formula</h5>
+                {!this.state.atoms && <p>Please check the formula{"'"}s syntax</p>}
+                {typeof this.state.atoms === 'string' && <p>The element "{this.state.atoms}" does not exist</p>}
+              </div>
+              :
+              this.state.hasAnswered && Object.keys(atoms).length > 0 &&
+                <div className="alert span5 alert-info">
+                  <h5>The molecule {this.state.sentFormula} is made of:</h5>
+                  <ul className="icons">
+                    {atomsList}
+                  </ul>
+                </div>
+            }
+            {
+              this.state.error && 
+              <div className="alert span5 alert-danger">
+                <h5>Server Error</h5>
+                <p>Teh following error has occured, please retry later: <em>{this.state.error}</em></p>
+              </div>
+            }
+          </form>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App
