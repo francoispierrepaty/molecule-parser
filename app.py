64fa751bd8087203b50236fe292bce4d96a06a4c
François-Pierre PATY <francoispierrepaty@MacBook-Pro-de-Francois-Pierre.local>
2021-04-16 12:33:13 +0200

    path

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 #from flask_cors import CORS #comment this on deployment
 from api.MolecularParser import MolecularParser
+import os
 
-app = Flask(__name__, static_url_path='', static_folder='molecular-parser/build')
+app = Flask(__name__, static_url_path='', static_folder=os.path.abspath('./molecular-parser/build'))
 #CORS(app) #comment this on deployment
 api = Api(app)
 
